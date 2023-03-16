6b42c91de3357da9a38761b5af20501b85d7b110
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2023-03-16 23:25:09 +0200

    Add files via upload

---
+++ Task2_1_17.03.23/Task2_1_17.03.23/Task2_1_17.03.23.cpp
@@ -0,0 +1,50 @@
+﻿/*Task1. In a one-dimensional array filled with random numbers, determine the minimum and maximum elements.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Main loop    
+    while (true) {
+
+        const int SIZE = 10; // Size of array
+        int arr[SIZE]; // Array of random numbers
+        int min_val = INT_MAX, max_val = INT_MIN; // Initialize min and max values
+
+        // Fill array with random numbers
+        for (int i = 0; i < SIZE; i++) {
+
+            arr[i] = rand(); // rand() generates a random number
+        }
+
+        // Iterate over array and update min and max values
+        for (int i = 0; i < SIZE; i++) {
+
+            if (arr[i] < min_val) {
+
+                min_val = arr[i];
+            }
+            if (arr[i] > max_val) {
+
+                max_val = arr[i];
+            }
+        }
+
+        // Display the minimum and maximum values
+        cout << "\nMinimum value: " << min_val << endl;
+        cout << "Maximum value: " << max_val << endl;
+
+        // Ask user if they want to see another result
+        int choice;
+        cout << "\nEnter 1 to see another result, 0 to exit: ";
+        cin >> choice;
+
+        // Exit loop
+        if (choice == 0) {
+
+            break; 
+
+        }
+    }
+}
---
+++ Task2_3_17.03.23/Task2_3_17.03.23/Task2_3_17.03.23.cpp
@@ -0,0 +1,92 @@
+﻿/*Task3. In a one-dimensional array consisting of N real numbers, calculate:
+- the sum of negative elements
+- the product of the elements located between the min and max elements
+- the product of elements with even numbers
+- the sum of elements located between the first and last negative elements*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int N = 10; // Size of the array
+    double arr[N]; // Array of N real numbers
+
+    // Input the elements of the array
+    cout << "Enter " << N << " real numbers: ";
+    for (int i = 0; i < N; i++) {
+
+        cin >> arr[i];
+    }
+
+    // Calculate the sum of negative elements
+    double sum_neg = 0;
+    for (int i = 0; i < N; i++) {
+
+        if (arr[i] < 0) {
+
+            sum_neg += arr[i];
+        }
+    }
+    cout << "Sum of negative elements: " << sum_neg << endl;
+
+    // Find the index of the minimum and maximum elements
+    int min_index = 0;
+    int max_index = 0;
+    for (int i = 1; i < N; i++) {
+
+        if (arr[i] < arr[min_index]) {
+
+            min_index = i;
+        }
+        if (arr[i] > arr[max_index]) {
+
+            max_index = i;
+        }
+    }
+
+    // Calculate the product of the elements between the min and max elements
+    long double prod_between = 1;
+    int start_index = min_index < max_index ? min_index : max_index; // The ? is the ternary operator, which is a shorthand way of writing an if-else statement
+    int end_index = min_index > max_index ? min_index : max_index;
+
+    for (int i = start_index + 1; i < end_index; i++) {
+
+        prod_between *= arr[i];
+    }
+    cout << "Product of elements between min and max: " << prod_between << endl;
+
+    // Calculate the product of elements with even indices
+    long double prod_even = 1;
+    for (int i = 0; i < N; i += 2) {
+
+        prod_even *= arr[i];
+    }
+    cout << "Product of elements with even indices: " << prod_even << endl;
+
+    // Calculate the sum of elements between the first and last negative elements
+    long double sum_between_neg = 0;
+    int first_neg_index = -1;
+    int last_neg_index = -1;
+
+    for (int i = 0; i < N; i++) {
+
+        if (arr[i] < 0) {
+
+            if (first_neg_index == -1) {
+
+                first_neg_index = i;
+            }
+            last_neg_index = i;
+        }
+    }
+    if (first_neg_index != -1 && last_neg_index != -1) {
+
+        for (int i = first_neg_index + 1; i < last_neg_index; i++) {
+
+            sum_between_neg += arr[i];
+        }
+    }
+    cout << "Sum of elements between first and last negative: " << sum_between_neg << endl;
+
+}
